689d36491b1970638a69aa3cf743e8826eef854d
Jovic Abengona <jvlabengona@gmail.com>
2021-07-01 09:52:58 +0800

    Initial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      flash[:notice] = ["Welcome, #{user_params[:first_name]}"]
+      redirect_to user_path(User.last)
+    else
+      flash[:notice] = @user.errors.full_messages
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
+end
